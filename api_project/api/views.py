6575fb5a6fa4c38d773910eb25494f62823f5a3f
shoile azeez <shoabdulazeez@gmail.com>
2024-08-30 21:56:46 +0100

    Implementing Authentication and Permissions in Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,20 @@
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookViewSet(APIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
 
-
+    def get(self, request):
+        # Only authenticated users can access this view
+        return Response({'message': 'Hello, authenticated user!'})
 # Create your views here.
